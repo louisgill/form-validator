4ff6d98cc7f3814f527de1f9ab89d251afcdd0d5
Louis Gill <lgill@cefcu.com>
2020-03-12 15:44:40 -0500

    Adding password match check

--- script.js
+++ script.js
@@ -50,6 +50,13 @@ function checkLength(input, min, max) {
     }
 }
 
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match');
+    }
+}
+
 // Get fieldname
 function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -63,4 +70,5 @@ form.addEventListener('submit', function(e) {
     checkLength(username, 3, 15);
     checkLength(password, 6, 25);
     checkEmail(email);
+    checkPasswordsMatch(password, password2);
 });
